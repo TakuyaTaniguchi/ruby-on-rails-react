71c110de9aee4cf0a2546a435140e409536c24a5
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-16 08:15:43 +0900

    必要なCRUDを用意する

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update destroy ]
+
   def index
     @users = User.all
     render json: @users
   end
-end
+
+  def show
+    # @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
+
+  def destroy
+    # @user = User.find(params[:id])
+    @user.destroy
+    render json: @user
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.permit(:name, :email, :password)
+    end
+  end
