3f4e49088f494006e4ae791346ee6c14c3c68845
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-16 08:09:55 +0900

    Usersを作成 Jsonを返すようにする

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_234635) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_15_225319) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
