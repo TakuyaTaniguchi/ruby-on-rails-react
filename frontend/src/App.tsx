1d7e2e5c7f327b45e4116cbedf32be3dedc231ec
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-17 15:26:15 +0900

    Routerを作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 import { MyPageComponent } from "./MyPage"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MyPageComponent />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <header className="App-header">
+            <Routes>
+              <Route path="/hoge" element={<div>Home</div>} />
+            </Routes>
+            <MyPageComponent />
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+      </Router>
     </div>
   );
 }
