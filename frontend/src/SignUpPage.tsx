b6d494dd319aa28276d4810869faa3f936df206c
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-20 23:11:44 +0900

    SingUpページを作成する

--- frontend/src/SignUpPage.tsx
+++ frontend/src/SignUpPage.tsx
@@ -3,10 +3,32 @@ import React, {} from 'react';
 
 export function SignUpPage() {
 
+  const singUp = () => {
+    console.log('singUp');
+  }
 
   return (
     <div>
-      <h1>Sign Up Page </h1>
+      <div className='sing-up'>
+        <h1 className='form-title'>Sign Up</h1>
+        <div className='form-wrap'>
+          <div className='form-section'>
+            <label className='form-label' htmlFor="email">Email</label>
+            <input className='form-input' type="email" id="email" name="email" />
+          </div>
+          <div className='form-section'>
+            <label className='form-label' htmlFor="password">Password</label>
+            <input className='form-input' type="password" id="password" name="password" />
+          </div>
+          <div className='form-section'>
+            <label className='form-label' htmlFor="passwordConfirmation">Password Confirmation</label>
+            <input className='form-input' type="password" id="passwordConfirmation" name="passwordConfirmation" />
+          </div>
+          <div className='form-section mt-20'>
+            <button onClick={singUp} className='form-submit' type="submit">Sign Up</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
