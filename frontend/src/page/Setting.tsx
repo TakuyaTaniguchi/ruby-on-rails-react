24a50b335fec1846dc831a3c8019d9d8513d9f34
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-23 23:15:57 +0900

    デザインを軽く整える

--- frontend/src/page/Setting.tsx
+++ frontend/src/page/Setting.tsx
@@ -1,7 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 // import { UsersComponent } from "./User";
 
 export function SettingPage() {
+
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [image, setImage] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [isEdiitng, setIsEditing] = useState(false);
+
+  const HTMLNameInput = useRef<HTMLInputElement>(null);
 
   const [user, setUser] = useState({
     id: 0,
@@ -30,13 +39,41 @@ export function SettingPage() {
       .then(response => response.json()) // レスポンスをJSONに変換
       .then(data => {
         setUser(data);
+        console.log(data);
+        setEmail(data.email);
+        setName(data.name);
+        setNickname(data.nickname);
+        setImage(data.image);
       })
       .catch(error => console.error('Error:', error));
-  })
+  },[])
 
-  // 変更
-  // authPUT
+  // ユーザー情報変更
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const updateUser = () => {
+    fetch(`http://localhost:3000/auth/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': localStorage.getItem('access-token') ||'',
+        'client': localStorage.getItem('client')||'',
+        'uid': localStorage.getItem('uid')||'',
+      },
+      body: JSON.stringify({ 
+        name: user.name,
+        nickname: user.nickname,
+        image: user.image,
+      }),
+    }).then(response => {
+      if (response.ok) {
+        console.log('Update Success');
+      } else {
+        console.log('Update Failed');
+      }
+    })
+  }
 
+// https://getavataaars.com/
 
   return (
     <div className='setting'>
@@ -44,21 +81,42 @@ export function SettingPage() {
         <h1 className='form-title'>Setting</h1>
       </div>
       <div className='form-wrap'>
-      <div className='section'>
-          <div className='head'>名前</div>
-          <div className='body'>{user.name}</div>
+        <div className='section'>
+            <div className='body icon'><img src={image} /></div>
         </div>
         <div className='section'>
-          <div className='head'>表示名</div>
-          <div className='body'>{user.nickname}</div>
+            <div className='head'>名前</div>
+            { 
+              isEdiitng ? ( 
+                <div className='body'>
+                  <input className='form-input' ref={HTMLNameInput} type="text" onChange={(e)=>{setName(e.target.value)}} value={name}></input>
+                </div>
+                ) : ( <div className='body'>{name}</div>)
+            }
         </div>
         <div className='section'>
-          <div className='head'>アイコン</div>
-          <div className='body'>{user.image}</div>
+          <div className='head'>ニックネーム</div>
+          {
+            isEdiitng ? (
+              <div className='body'>
+                <input className='form-input' type="text" onChange={(e)=>{setNickname(e.target.value)}} value={nickname}></input>
+              </div>
+              ) : ( <div className='body'>{nickname}</div>)
+          }
         </div>
         <div className='section'>
           <div className='head'>メールアドレス</div>
-          <div className='body'>{user.email}</div>
+          {
+            isEdiitng ? (
+              <div className='body'>
+                <input className='form-input' type="text" onChange={(e)=>{setEmail(e.target.value)}} value={email}></input>
+              </div>
+              ) : ( <div className='body'>{email}</div>)
+          }
+        </div>
+        <div className='buttin-wrap mt-20'>
+          <button className='form-submit harf' onClick={() =>{setIsEditing(!isEdiitng)}}>Edit</button>
+          <button className='form-submit harf' onClick={() =>{setIsEditing(!isEdiitng)}}>Update</button>
         </div>
       </div>
     </div>
