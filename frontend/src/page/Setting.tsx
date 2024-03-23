9bb4172a92f1688c6b4e5430cfbc4383b708bd1c
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-23 09:38:08 +0900

    userを表示

--- frontend/src/page/Setting.tsx
+++ frontend/src/page/Setting.tsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { UsersComponent } from "./User";
 
 export function SettingPage() {
 
-  const nickname = 'nickname';
-  const mailAddress = 'mailAddress';
-  const icon = 'icon';
+  const [user, setUser] = useState({
+    id: 0,
+    email: '',
+    provider: '',
+    uid: '',
+    allow_password_change: false,
+    name: '',
+    nickname: '',
+    image: '',
+    created_at: '',
+    updated_at: '',
+    memos: [],
+  });
 
   useEffect(() => {
     fetch('http://localhost:3000/users/me',{
@@ -19,29 +29,36 @@ export function SettingPage() {
     })
       .then(response => response.json()) // レスポンスをJSONに変換
       .then(data => {
-        console.log(data); // データをセット
+        setUser(data);
       })
       .catch(error => console.error('Error:', error));
   })
+
+  // 変更
+  // authPUT
 
 
   return (
     <div className='setting'>
       <div>
-        <h1>Setting</h1>
+        <h1 className='form-title'>Setting</h1>
       </div>
       <div className='form-wrap'>
+      <div className='section'>
+          <div className='head'>名前</div>
+          <div className='body'>{user.name}</div>
+        </div>
         <div className='section'>
           <div className='head'>表示名</div>
-          <div className='body'>{nickname}</div>
+          <div className='body'>{user.nickname}</div>
         </div>
         <div className='section'>
           <div className='head'>アイコン</div>
-          <div className='body'>{icon}</div>
+          <div className='body'>{user.image}</div>
         </div>
         <div className='section'>
           <div className='head'>メールアドレス</div>
-          <div className='body'>{mailAddress}</div>
+          <div className='body'>{user.email}</div>
         </div>
       </div>
     </div>
