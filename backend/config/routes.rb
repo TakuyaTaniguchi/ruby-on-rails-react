360ecd72300f6ac074c6c9626cd648489c12de85
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-25 19:45:42 +0900

    一旦サンプルでgraphQlのリクエストができるようにする

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :groups
   resources :users
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,13 +14,33 @@ import './App.css';
 // util
 import { isLogin } from './util/isLogin'
 
+import { gql, useQuery } from '@apollo/client';
 
 export const loginContext = createContext(isLogin());
 
+// GraphQLクエリとその結果の型を定義
+const TEST_QUERY = gql`
+  query TestQuery {
+    testSample,
+    testField
+  }
+`;
+
+
+
+type TestQueryResult = {
+  testSample: string;
+  testField: string;
+};
 
 function App() {
-  
+  const { loading, error, data } = useQuery<TestQueryResult>(TEST_QUERY);
   const login = useContext(loginContext);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
 
   return (
     <div className="App">
@@ -28,6 +48,7 @@ function App() {
         <Router>
             <Header/>
             <div className='App-body'>
+            <div>{data?.testSample}:apollo</div>
               <Routes>
                 <Route path="/memos" element={<MemoPage/>} />
                 <Route path="/sign_up" element={<SignUpPage/>} />
