76d628bc358ea6182aa25176fc7447ecbd35f5f1
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-06-24 16:57:49 +0900

    delete

--- frontend/src/page/Payment.tsx
+++ frontend/src/page/Payment.tsx
@@ -1,6 +1,6 @@
 import React, { } from 'react';
 import { LayoutStyleDiv } from '../css/design';
-import  AmountSummary from '../AmountSummary';
+// import  AmountSummary from '../AmountSummary';
 
 
 export function PaymentPage() {
@@ -8,13 +8,13 @@ export function PaymentPage() {
     <LayoutStyleDiv>
       <div>
         <h1>Payment</h1>
-        <AmountSummary
+        {/* <AmountSummary
           text='クレジットカード'
           price={1000}
           discount={100}
           isSlectedCoupon={true}
           couponDiscount={100}
-        />
+        /> */}
       </div>
     </LayoutStyleDiv>
   );
